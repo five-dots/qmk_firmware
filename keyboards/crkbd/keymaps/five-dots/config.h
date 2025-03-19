aeeba789661f8c741765468b9a52ca1076e452ab
Shun Asai <shun.asai@tmkc.co.jp>
2025-03-19 14:06:19 +0900

    Update crkbd/five-dots

--- keyboards/crkbd/keymaps/five-dots/config.h
+++ keyboards/crkbd/keymaps/five-dots/config.h
@@ -22,37 +22,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define USE_MATRIX_I2C
 
-/* Select hand configuration */
-
-#define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
-
-#define USE_SERIAL_PD2
-
-// Mod Tap key を Tap 後にすぐに Hold したときに、Hold として扱われるようになる
-#define TAPPING_FORCE_HOLD
-
-#define TAPPING_TERM 200 // Default 200
-
-// TAPPING_TERM を経過しても、他のキーを押していなければ Tap として扱われるようになる
-// TAPPING_TERM が短くて Tap のつもりが Hold 扱いになってしまう場合の対策になる
+//#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 100
 #define RETRO_TAPPING
 
-// TAPPING_TERM 経過前に他のキーを押すと、Hold として扱われるようになる
-#define PERMISSIVE_HOLD
-
-// Mod Tap key と通常キーを連続して高速に押したとき、両方のキーを Tap として扱うようになる
-#define IGNORE_MOO_TAP_INTERRUPT
-
 #ifdef RGBLIGHT_ENABLE
-    #undef RGBLED_NUM
-    #define RGBLIGHT_ANIMATIONS
-    #define RGBLED_NUM 27
+    #define RGBLIGHT_EFFECT_BREATHING
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    #define RGBLIGHT_EFFECT_SNAKE
+    #define RGBLIGHT_EFFECT_KNIGHT
+    #define RGBLIGHT_EFFECT_CHRISTMAS
+    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #define RGBLIGHT_EFFECT_RGB_TEST
+    #define RGBLIGHT_EFFECT_ALTERNATING
+    #define RGBLIGHT_EFFECT_TWINKLE
     #define RGBLIGHT_LIMIT_VAL 120
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
 #endif
-
-#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
